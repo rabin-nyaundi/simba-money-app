0abda077c19ccace4ca65e9270e45bd8f4f10303
rabin-nyaundi <nyaundirabin@gmail.com>
2021-11-24 15:40:45 +0300

    fix Database_url

--- lib/prisma.jsx
+++ lib/prisma.jsx
@@ -1,15 +1,17 @@
 import { PrismaClient } from "@prisma/client";
-let prisma;
+// let prisma;
 
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient()
-}
+// if (process.env.NODE_ENV === 'production') {
+//     prisma = new PrismaClient()
+// }
 
-else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient()
-    }
-    prisma = global.prisma
-}
+// else {
+//     if (!global.prisma) {
+//         global.prisma = new PrismaClient()
+//     }
+//     prisma = global.prisma
+// }
+
+let prisma = new PrismaClient();
 
 export default prisma;
