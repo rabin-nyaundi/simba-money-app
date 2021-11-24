149f4163d9fb07c4aa6cfd48baa0227cd5e2c2c7
rabin-nyaundi <nyaundirabin@gmail.com>
2021-11-24 20:55:16 +0300

    fix

--- lib/prisma.jsx
+++ lib/prisma.jsx
@@ -1,16 +1,16 @@
 import { PrismaClient } from "@prisma/client";
-let prisma;
+const prisma = new PrismaClient();
 
-if (process.env.NODE_ENV === 'production') {
-    prisma = new PrismaClient()
-}
+// if (process.env.NODE_ENV === 'production') {
+//     prisma = new PrismaClient()
+// }
 
-else {
-    if (!global.prisma) {
-        global.prisma = new PrismaClient()
-    }
-    prisma = global.prisma
-}
+// else {
+//     if (!global.prisma) {
+//         global.prisma = new PrismaClient()
+//     }
+//     prisma = global.prisma
+// }
 
 
 
