cb93a5a145fccbf305bceaed20bd06dc95db2874
rabin-nyaundi <nyaundirabin@gmail.com>
2021-11-25 09:57:57 +0300

    fix auth

--- pages/api/auth/[...nextauth].jsx
+++ pages/api/auth/[...nextauth].jsx
@@ -5,29 +5,36 @@ import { compare } from "bcryptjs";
 
 let userAccount;
 
-
 const options = {
   providers: [
     CredentialsProvider({
       name: "credentials",
       async authorize(credentials) {
-        const user = await prisma.user.findUnique({
-          where: { email: credentials.email },
-        });
-        if (!user) {
-          throw new Error("User with email not found");
-        }
-        const checkPassword = await compare(
-          credentials.password,
-          user.password
-        );
+        try {
+          const user = await prisma.user.findUnique({
+            where: { email: credentials.email },
+          });
 
-        if (!checkPassword) {
-          throw new Error("Invalid password");
+          if (!user) {
+            throw new Error("User with email not found");
+          }
+          const checkPassword = await compare(
+            credentials.password,
+            user.password
+          );
+
+          if (!checkPassword) {
+            throw new Error("Invalid password");
+          }
+
+          userAccount = user;
+          console.log(userAccount);
+          return Promise.resolve(user);
+
+        } catch (error) {
+          throw new Error(error);
         }
-        userAccount = user;
-        console.log(userAccount);
-        return Promise.resolve(user)
+
       },
     }),
   ],
@@ -36,23 +43,25 @@ const options = {
     async session(session, token) {
       if (userAccount !== null) {
         session.user = userAccount;
-      }
-      else if (typeof token.user !== typeof undefined && (typeof session.user === typeof undefined
-        || (typeof session.user !== typeof undefined && typeof session.user.userId === typeof undefined))) {
+      } else if (
+        typeof token.user !== typeof undefined &&
+        (typeof session.user === typeof undefined ||
+          (typeof session.user !== typeof undefined &&
+            typeof session.user.userId === typeof undefined))
+      ) {
         session.user = token.user;
-      }
-      else if (typeof token !== typeof undefined) {
+      } else if (typeof token !== typeof undefined) {
         session.token = token;
       }
       return session;
     },
-    // async jwt({ token, user }) {
-    //   if (user) {
-    //     token.user =  user
-    //   }
-    //   return token;
-    // },
-  
+    
+    async jwt({ token, user }) {
+      if (user) {
+        token.user = user
+      }
+      return token;
+    },
   },
 
   session: {
@@ -63,8 +72,7 @@ const options = {
   pages: {
     error: "/auth/login",
     signIn: "/auth/login",
-    signOut: "/auth/login"
+    signOut: "/auth/login",
   },
-}
-export default (req, res) => NextAuth( req, res, options);
-
+};
+export default (req, res) => NextAuth(req, res, options);
--- pages/auth/login.jsx
+++ pages/auth/login.jsx
@@ -1,42 +1,30 @@
-import { ToastContainer } from "react-toastify";
+import { toast, ToastContainer } from "react-toastify";
 import LoginForm from "../../components/auth/LoginForm";
 import { useSession, signIn, signOut, getSession } from "next-auth/react";
 import { Router, useRouter } from "next/dist/client/router";
 
 
-export default function Login() {
+export default function Login({session}) {
 
-  const { data: session, status } = useSession();
-  const router = useRouter();
-
-  if (status === 'authenticated') {
-    router.push('/');
-    return;
-  }
 
   async function handleLogin(data) {
     let email = data.email;
     let password = data.password;
-    const result = await signIn("credentials", {
+    const res = await signIn("credentials", {
       email,
       password,
-      callbackUrl: "https://simba-money-transfer-web-app.vercel.app/",
-    });
-    return result;
+      callbackUrl: `${window.location.origin}/transact`,
+      // redirect: false
+    })
+    if (res?.error) {
+      toast.error(res.error);
+      console.log(res.error);
+    }
+    
+    return res;
   }
   return (
     <>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
       <div className="flex flex-col mt-10 py-8 px-8 mx-auto items-center">
         <LoginForm handleLogin={handleLogin} />
       </div>
